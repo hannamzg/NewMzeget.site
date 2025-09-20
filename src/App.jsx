c4a1d1f76edeccb33a28573483df132578df6845
hannnamzg <elimzghanna123@gmail.com>
2025-09-20 17:55:46 +0300

    ==

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import FloatingContactButton from './components/FloatingContactButton';
+import ScrollToTop from './components/ScrollToTop';
 
 // Pages
 import Home from './pages/Home';
@@ -25,6 +26,7 @@ import FAQ from './pages/FAQ';
 function App() {
   return (
     <div className="min-h-screen flex flex-col">
+      <ScrollToTop />
       <Header />
       <main className="flex-grow">
             <Routes>
